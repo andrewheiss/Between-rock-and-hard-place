bc5f211122c1c92928ff71456e260bb9b3dfb2a8
Andrew Heiss <andrew@andrewheiss.com>
2016-05-16 17:45:37 -0400

    Get, clean, and graph DCJW data

---
+++ analysis/figures.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(feather)
+library(ggplot2)
+
+dcjw <- read_feather(file.path(PROJHOME, "data", "dcjw.feather"))
+
+# Different types of civil society restrictions over time
+restrictions <- dcjw %>%
+  filter(question %in% c("q_2a", "q_3e", "q_4a"), !is.na(year)) %>%
+  arrange(year.actual) %>%
+  group_by(question, year.actual) %>%
+  summarise(count = n()) %>%
+  mutate(cumulative.count = cumsum(count)) %>%
+  ungroup() %>%
+  mutate(question = factor(question, levels=c("q_2a", "q_3e", "q_4a"),
+                           labels=c("Registration requirements    ",
+                                    "Foreign funding prohibition    ",
+                                    "Advocacy restrictions")))
+
+ggplot(restrictions, aes(x=year.actual, y=cumulative.count,
+                         colour=question, linetype=question)) + 
+  geom_line(size=1) + 
+  coord_cartesian(xlim=ymd("1950-01-01", "2011-01-01")) +
+  labs(x=NULL, y="Number of countries with law") + 
+  scale_color_manual(values=c("black", "grey50", "grey30"), name=NULL) +
+  scale_linetype_manual(values=c("solid", "solid", "32"), name=NULL) +
+  theme_light(8) + theme(legend.position="bottom")
---
+++ analysis/get_clean_data.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(tidyr)
+library(readr)
+library(readxl)
+library(lubridate)
+library(countrycode)
+library(feather)
+
+# -----------------------------------------------------------------------------
+# NB: See `data/external_data.yaml` for metadata about each of these datasets
+# -----------------------------------------------------------------------------
+
+# ------------------------------------
+# Christensen and Weinstein NGO laws
+# ------------------------------------
+# Get and load DCJW data
+dcjw.url <- "https://darinchristensen.github.io/Data/DCJW_NGO_Laws.xlsx"
+dcjw.name <- basename(dcjw.url)
+dcjw.path <- file.path(PROJHOME, "data_raw", dcjw.name)
+
+if (!file.exists(dcjw.path)) {
+  download.file(url=dcjw.url, dcjw.path, "auto")
+}
+
+# Tidy DCJW data
+dcjw <- read_excel(dcjw.path)[,1:50] %>%
+  select(-c(contains("source"), contains("burden"), contains("subset"), Coder, Date)) %>%
+  gather(key, value, -Country) %>%
+  separate(key, c("question", "var.name"), 4) %>%
+  filter(!is.na(Country)) %>%
+  mutate(var.name = ifelse(var.name == "", "value", gsub("_", "", var.name))) %>%
+  spread(var.name, value) %>%
+  mutate(year.actual = ymd(paste0(year, "-01-01")),
+         country.name = countrycode(Country, "country.name", "country.name"))
+
+write_feather(dcjw, file.path(PROJHOME, "data", "dcjw.feather"))
