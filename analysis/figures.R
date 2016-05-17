d10a9741e239f5f0c0d37699604f4e7513351948
Andrew Heiss <andrew@andrewheiss.com>
2016-05-16 22:40:00 -0400

    Better y-axis label

--- analysis/figures.R
+++ analysis/figures.R
@@ -21,7 +21,10 @@ ggplot(restrictions, aes(x=year.actual, y=cumulative.count,
                          colour=question, linetype=question)) + 
   geom_line(size=1) + 
   coord_cartesian(xlim=ymd("1950-01-01", "2011-01-01")) +
-  labs(x=NULL, y="Number of countries with law") + 
+  labs(x=NULL, y="Cumulative number of countries with law") + 
+  scale_color_manual(values=c("black", "grey50", "grey30"), name=NULL) +
+  scale_linetype_manual(values=c("solid", "solid", "32"), name=NULL) +
+  theme_light(8) + theme(legend.position="bottom")
   scale_color_manual(values=c("black", "grey50", "grey30"), name=NULL) +
   scale_linetype_manual(values=c("solid", "solid", "32"), name=NULL) +
   theme_light(8) + theme(legend.position="bottom")
